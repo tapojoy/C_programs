398114a71b615d6b501353d5fc3a0cdef249b5c8
tapojoy <194054174+tapojoy@users.noreply.github.com>
2026-06-12 08:11:48 +0530

    add 13_3_structs_complex_numbers_typedef

---
+++ 13_3_structs_complex_numbers_typedef.c
@@ -0,0 +1,37 @@
+#include "stdio.h"
+
+typedef struct {
+    float x;
+    float y;
+} complex;
+
+void complex_add(complex, complex, complex *);
+
+int main() {
+    complex c1, c2, c;
+    printf("Enter real and imaginary part of 1st number:: ");
+    scanf("%f%f",&c1.x,&c1.y);
+    
+    printf("Enter real and imaginary part of 2nd number:: ");
+    scanf("%f%f",&c2.x,&c2.y);
+    
+    complex_add(c1, c2, &c);
+        
+    printf(
+        "Sum of %5.2f + (%5.2fi) and %5.2f + (%5.2fi) = %5.2f + (%5.2fi)\n",
+        c1.x,
+        c1.y,
+        c2.x,
+        c2.y,
+        c.x,
+        c.y
+    );
+        
+    printf("\n");
+}
+
+void complex_add(complex c1, complex c2, complex *c){
+    c->x = c1.x+c2.x;
+    c->y = c1.y+c2.y;
+}
+
