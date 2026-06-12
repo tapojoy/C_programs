e4eafd474537559ba427b32ac7d66f89fdb40382
tapojoy <194054174+tapojoy@users.noreply.github.com>
2026-06-12 08:20:44 +0530

    update 13_3_structs_complex_numbers_typedef
    - pass addresses to avoid passing entire structs
    - add comments explaining the changes

--- 13_3_structs_complex_numbers_typedef.c
+++ 13_3_structs_complex_numbers_typedef.c
@@ -5,7 +5,7 @@ typedef struct {
     float y;
 } complex;
 
-void complex_add(complex, complex, complex *);
+void complex_add(complex *,complex *,complex *);
 
 int main() {
     complex c1, c2, c;
@@ -15,8 +15,11 @@ int main() {
     printf("Enter real and imaginary part of 2nd number:: ");
     scanf("%f%f",&c2.x,&c2.y);
     
-    complex_add(c1, c2, &c);
-        
+    complex_add(&c1, &c2, &c);
+    // passing the address of c1 and c2 is not required
+    // as only c requires call by value
+    // but it works  
+            
     printf(
         "Sum of %5.2f + (%5.2fi) and %5.2f + (%5.2fi) = %5.2f + (%5.2fi)\n",
         c1.x,
@@ -30,8 +33,10 @@ int main() {
     printf("\n");
 }
 
-void complex_add(complex c1, complex c2, complex *c){
-    c->x = c1.x+c2.x;
-    c->y = c1.y+c2.y;
+void complex_add(complex *c1, complex *c2, complex *c){
+    // using '->' is required in lieu of '.' when addresses are passed
+    // as in case of call by value
+    c->x = c1->x+c2->x;
+    c->y = c1->y+c2->y;
 }
 
